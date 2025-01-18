201e8c5134646047f071c6d259cd77920305106c
ADRIANO SENA <61697830+AdrianoSenaS@users.noreply.github.com>
2025-01-18 12:54:43 -0300

    Finalizando SignalR e tomei café

--- Server/Hubs/WebRTCSignalingHub.cs
+++ Server/Hubs/WebRTCSignalingHub.cs
@@ -1,6 +1,15 @@
-﻿namespace CamWebRtc.Server.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CamWebRtc.Server.Hubs
 {
-    public class WebRTCSignalingHub
+    public class WebRTCSignalingHub : Hub
     {
+        public async Task SendOffer(string offer, string cameraId) => await Clients.Client(cameraId).SendAsync("ReceiveOffer", offer);
+
+        public async Task SendAnswer(string answer, string cameraId) => await Clients.Client(cameraId).SendAsync("ReceiveAnswer", answer);
+
+        public async Task SendIceCandidate(string candidate, string cameraId) => await Clients.Client(cameraId).SendAsync("ReceiveIceCandidate", candidate);
+
+        
     }
 }
