07e9a8329a6ef6036e0cfc7bdb601e85b5c43e82
ADRIANO SENA <61697830+AdrianoSenaS@users.noreply.github.com>
2025-01-30 16:37:04 -0300

    Delete wwwroot directory

--- wwwroot/assets/js/app/cookies.js
+++
@@ -1,6 +0,0 @@
-const Cookies = () => {
-    const cookies = document.cookie;
-    const tokenMatch = cookies.match(/(^|;) ?token=([^;]*)(;|$)/);
-    const token = tokenMatch ? tokenMatch[2] : null;
-    return token
-}
